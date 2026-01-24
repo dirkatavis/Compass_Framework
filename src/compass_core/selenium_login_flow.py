e0780e2979c6360c54e8c37f58f9a16c12f1ca2b
Dirk Steele <dirk.avis@gmail.com>
2026-01-24 07:37:57 -0500

    Minor fix for several syntax issues

--- src/compass_core/selenium_login_flow.py
+++ src/compass_core/selenium_login_flow.py
@@ -5,6 +5,7 @@ Handles Microsoft authentication flow with username/password entry.
 """
 from typing import Dict, Any
 import logging
+import time
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
